219f45d17a8a390310a612b794009b8a229c886e
Mysteriosis <pacurty@gmail.com>
2018-03-13 00:55:38 +0100

    Add stops route

--- app.py
+++ app.py
@@ -5,7 +5,25 @@ app = Flask(__name__)
 BASE_URL = "https://www.travys.ch/wp-json/travys_tiramisu/v1/iv/lines"
 ICON = "i19110"
 
+def getData(line, stop, direction):
+    url = BASE_URL + "/{}/{}".format(line, direction)
+    http = urllib3.PoolManager()
+    headers = urllib3.util.make_headers(keep_alive=True, accept_encoding=True, user_agent="LameEtTrique/1.0")
+    req = http.request('GET', url, headers=headers)
+    
+    return json.loads(req.data.decode('utf-8'))['line']['stops']
+
+def getPayload(values):
+    payload = { "frames": [] }
+    for value in values:
+        frame = { "text": "", "icon": ICON }
+        frame['text'] = value
+        payload['frames'].append(frame)
+
+    return payload
+
 @app.route("/")
+@app.route("/schedule")
 def lame_et_trique():
     texts = []
     schedule = dict()
@@ -17,11 +35,7 @@ def lame_et_trique():
         texts.append("Données fournies incorrectes.")
     else:
         direction = request.args.get('direction') if not request.args.get('direction') else 'forward'
-        url = BASE_URL + "/{}/{}".format(line, direction)
-        http = urllib3.PoolManager()
-        headers = urllib3.util.make_headers(keep_alive=True, accept_encoding=True, user_agent="LameEtTrique/1.0")
-        req = http.request('GET', url, headers=headers)
-        json_file = json.loads(req.data.decode('utf-8'))['line']['stops']
+        json_file = getData(line, stop, direction)
 
         for value in json_file:
             if str(value['name']) == str(stop):
@@ -34,13 +48,29 @@ def lame_et_trique():
         else:
             texts.append("Pas de correspondance pour la ligne {}, à l'arrêt {}".format(line, stop))
 
-    payload = { "frames": [] }
-    for value in texts:
-        frame = { "text": "", "icon": ICON }
-        frame['text'] = value
-        payload['frames'].append(frame)
+    return(json.dumps(getPayload(texts)))
 
-    return(json.dumps(payload))
+@app.route("/stops")
+def get_stops():
+    stops = ""
+
+    line = request.args.get('line')
+    stop = request.args.get('stop')
+
+    if not line or not stop:
+        return 'Données manquantes !', 422
+
+    direction = request.args.get('direction') if not request.args.get('direction') else 'forward'
+    json_file = getData(line, stop, direction)
+
+    for value in json_file:
+        stops.append(str(value['name']) + "<br />")
+
+    if stops == "":
+        return "Pas d'arrêt pour la ligne {} dans le sens {}".format(line, direction)
+
+    return(stops)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
