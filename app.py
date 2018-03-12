7513938666b287965cdae1578e5917dc8f550ee7
Mysteriosis <pacurty@gmail.com>
2018-03-13 00:04:30 +0100

    Fix missing parameters

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def lame_et_trique():
         frame['text'] = value
         payload['frames'].append(frame)
 
-    return(str(payload))
+    return(json.dump(payload))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
