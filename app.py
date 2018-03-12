6ece2b74d7a7cfcf9517ff1bd6eee8a4eea18fd6
Mysteriosis <pacurty@gmail.com>
2018-03-12 18:17:12 +0100

    App ok

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+import urllib3, json, ssl
+
+BASE_URL = "https://www.travys.ch/wp-json/travys_tiramisu/v1/iv/lines"
+ICON = "i19110"
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    line = request.args.get('line')
+    direction = request.args.get('direction') if len(request.args.get('direction')) > 0 else 'forward'
+    stop = request.args.get('stop')
+    url = BASE_URL + "/{}/{}".format(line, direction)
+
+    schedule = dict()
+    http = urllib3.PoolManager()
+    headers = urllib3.util.make_headers(keep_alive=True, accept_encoding=True, user_agent="LameEtTrique/1.0")
+    req = http.request('GET', url, headers=headers)
+    json_file = json.loads(req.data.decode('utf-8'))['line']['stops']
+
+    for value in json_file:
+        if str(value['name']) == str(stop):
+            schedule = value
+            break
+
+    texts = []
+    if len(schedule) > 0 and len(schedule['next_departures']) > 0:
+        texts.append("Ligne {}, arrêt {}".format(line, stop))
+        texts.append("Prochain bus dans {} min".format(schedule['next_departures'][0]['calculatedValue'])) 
+    else:
+        texts.append("Pas de correspondance pour la ligne {}, à l'arrêt {}".format(line, stop))
+
+    payload = { "frames": [] }
+    for value in texts:
+        frame = { "text": "", "icon": ICON }
+        frame['text'] = value
+        payload['frames'].append(frame)
+
+    return(str(payload))
+
+if __name__ == '__main__':
+   app.run(debug = True)
