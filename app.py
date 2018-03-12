9a1951d3a7f810a161d382bd2cebd7bb4e5acd84
Mysteriosis <pacurty@gmail.com>
2018-03-13 00:08:24 +0100

    Fix this shit

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def lame_et_trique():
         frame['text'] = value
         payload['frames'].append(frame)
 
-    return(json.dump(payload))
+    return(json.dumps(payload))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
